c27393d41825375a081b8ff7167209a8d3393ae1
Pedro Meyer <meyer.pedro@gmail.com>
2021-01-11 16:42:28 -0700

    src/plot.py edited

--- src/plots.py
+++ src/plots.py
@@ -112,66 +112,61 @@ y = pd.Series({'6':13865039.19,
                 '4':13794715.81973467}).sort_values(ascending=False)
 
 
-# ax.bar(y.keys(), y, alpha=0.7)
+ax.bar(y.keys(), y, alpha=0.7)
 
-# ax.set_title('LDA LogLikelihood Score vs K Value')
+ax.set_title('LDA LogLikelihood Score vs K Value')
 
-# ax.set_xlabel('K Value', fontsize=13)
-# ax.set_ylabel('LogLikelihood Score', fontsize=13)
+ax.set_xlabel('K Value', fontsize=13)
+ax.set_ylabel('LogLikelihood Score', fontsize=13)
 
 
-# ax.set_yscale('log')
+ax.set_yscale('log')
 
-# ax.legend()
+ax.legend()
 # plt.show()
 
-# plt.savefig('../images/lda_score.png')
-
-
-
-
 # Perplexity Score
-# fig, ax = plt.subplots(figsize=(10,6), dpi=500)
+fig, ax = plt.subplots(figsize=(10,6), dpi=500)
 
-# y = pd.Series({'4':2018.95,
-#                 '5':2064.25,
-#                 '6':2098.82,
-#                 '8':2115.45,
-#                 '10':2117.67,
-#                 '12':2118.41}).sort_values(ascending=False)
-
-
-# ax.bar(y.keys(), y, alpha=0.7, color='purple')
-
-# ax.set_title('LDA Perplexity Score vs K Value')
-
-# ax.set_xlabel('K Value', fontsize=13)
-# ax.set_ylabel('Perplexity Score', fontsize=13)
+y = pd.Series({'4':2018.95,
+                '5':2064.25,
+                '6':2098.82,
+                '8':2115.45,
+                '10':2117.67,
+                '12':2118.41}).sort_values(ascending=False)
 
 
-# ax.set_yscale('log')
+ax.bar(y.keys(), y, alpha=0.7, color='purple')
 
-# plt.savefig('../images/lda_perplexity.png')
+ax.set_title('LDA Perplexity Score vs K Value')
+
+ax.set_xlabel('K Value', fontsize=13)
+ax.set_ylabel('Perplexity Score', fontsize=13)
+
+
+ax.set_yscale('log')
+
+plt.savefig('../images/lda_perplexity.png')
 # plt.show();
 
 # Learning_decay Score
 
-# fig, ax = plt.subplots(figsize=(10,6), dpi=500)
+fig, ax = plt.subplots(figsize=(10,6), dpi=500)
 
-# y = pd.Series({'0.5':2236.7318894976393,
-#                 '1.0':2130.2805347555213,
-#                 '0.7':2064.253798134296}).sort_values(ascending=False)
-
-
-# ax.bar(y.keys(), y, alpha=0.7, color='green')
-
-# ax.set_title('LDA Perplexity Score vs Learning Rate Value')
-
-# ax.set_xlabel('Learning Rate Value', fontsize=13)
-# ax.set_ylabel('Perplexity Score', fontsize=13)
+y = pd.Series({'0.5':2236.7318894976393,
+                '1.0':2130.2805347555213,
+                '0.7':2064.253798134296}).sort_values(ascending=False)
 
 
-# ax.set_yscale('log')
+ax.bar(y.keys(), y, alpha=0.7, color='green')
 
-# plt.savefig('../images/lda_learningdecay.png')
+ax.set_title('LDA Perplexity Score vs Learning Rate Value')
+
+ax.set_xlabel('Learning Rate Value', fontsize=13)
+ax.set_ylabel('Perplexity Score', fontsize=13)
+
+
+ax.set_yscale('log')
+
+plt.savefig('../images/lda_learningdecay.png')
 # plt.show();
